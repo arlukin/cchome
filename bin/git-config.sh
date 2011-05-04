87d0f329622be9cab6d0413fecf0c25cd9268977
Daniel Lindh <daniel@cybercow.se>
2011-05-04 09:42:42 +0200

    New mount strucutre

--- bin/git-config.sh
+++ bin/git-config.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-git config --global user.name "Daniel Lindh"
+git config --global user.name "Arlukin"
 git config --global user.email daniel@cybercow.se
+
+git config --global diff.tool araxis
+git config --global merge.tool araxis
+
+echo "export PATH=$PATH:$HOME/cc/bin" >> $HOME/.bash_profile
+echo "export PATH=$PATH:$HOME/cc/bin/araxis" >> $HOME/.bash_profile
--- bin/mount.sh
+++ bin/mount.sh
@@ -4,21 +4,26 @@
 # Used on the office iMac to ssh mount directories on the office servers.
 #
 
+MOUNT=~/cc/mount
+
 function init_password_less_ssh {
   echo "Mount server " $1
-  ssh root@$1 'mkdir -p .ssh;chmod 700 .ssh; touch .ssh/authorized_keys;chmod 640 .ssh/authorized_keys'
-  cat /Users/dali/.ssh/id_rsa.pub | ssh root@$1 'cat >> .ssh/authorized_keys'
+  ssh root@$1 'mkdir -p .ssh;chmod 700 .ssh; touch ~/.ssh/authorized_keys;chmod 640 ~/.ssh/authorized_keys'
+  cat ~/.ssh/id_rsa.pub | ssh root@$1 'cat >> ~/.ssh/authorized_keys'
 }
 
 function ssh_mount_server {
   init_password_less_ssh $2
 
-  mkdir -p ~/mount/$1
-  umount ~/mount/$1
-  sshfs root@$2:/opt/ ~/mount/$1 -oauto_cache,reconnect
+  mkdir -p $MOUNT/$1
+  umount $MOUNT/$1
+  sshfs root@$2:/opt/ $MOUNT/$1 -oauto_cache,reconnect
 }
 
 #                 Folder            Server ip
-ssh_mount_server 'fp-tp-scan'      '10.100.100.12'
-ssh_mount_server 'fp-tp-gf-rd-int' '10.100.100.130'
+
+ssh_mount_server 'fo-tp-dalitst'   '10.100.100.231'
+ssh_mount_server 'fo-tp-php-old'   '10.100.0.100'
+ssh_mount_server 'fo-tp-file'      '10.100.0.4'
+ssh_mount_server 'fo-tp-vh01'      '10.100.100.201'
 ssh_mount_server 'fo-tp-install'   '10.100.100.200'
