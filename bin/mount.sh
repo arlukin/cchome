1a1bed7fab6cad16d8965bb9a4bc34138cd66b79
Arlukin <daniel@cybercow.se>
2011-05-17 12:12:07 +0200

    Updated mount functions, possible to use different users

--- bin/mount.sh
+++ bin/mount.sh
@@ -8,25 +8,25 @@ MOUNT=~/cchome/mount
 
 function init_password_less_ssh {
   echo "Mount server " $1
-  ssh root@$1 'mkdir -p .ssh;chmod 700 .ssh; touch ~/.ssh/authorized_keys;chmod 640 ~/.ssh/authorized_keys'
-  cat ~/.ssh/id_rsa.pub | ssh root@$1 'cat >> ~/.ssh/authorized_keys'
+  ssh $1 'mkdir -p .ssh;chmod 700 .ssh; touch ~/.ssh/authorized_keys;chmod 640 ~/.ssh/authorized_keys'
+  cat ~/.ssh/id_rsa.pub | ssh $1 'cat >> ~/.ssh/authorized_keys'
 }
 
 function ssh_mount_server {
-  echo "root@$2:$3 $MOUNT/$1" 
+  echo "$2:$3 $MOUNT/$1" 
   init_password_less_ssh $2
 
   mkdir -p $MOUNT/$1
   umount $MOUNT/$1
   #diskutil umount force $MOUNT/$1
-  sshfs root@$2:$3 $MOUNT/$1 
+  sshfs $2:$3 $MOUNT/$1 
   #-oauto_cache,reconnect
 }
 
 #                 Local Folder     Server ip          Remote folder
 
-ssh_mount_server 'fo-tp-dalitst'   '10.100.100.231'   '/opt/'
-ssh_mount_server 'fo-tp-php-old'   '10.100.0.100'     '/opt/RootLive/'
-ssh_mount_server 'fo-tp-file'      '10.100.0.4'       '/file/'
-ssh_mount_server 'fo-tp-vh01'      '10.100.100.201'   '/opt/'
-ssh_mount_server 'fo-tp-install'   '10.100.100.200'   '/opt/'
+ssh_mount_server 'fo-tp-dalitst'   'root@10.100.100.231'      '/opt/'
+ssh_mount_server 'fo-tp-php-old'   'root@10.100.0.100'        '/opt/RootLive/'
+ssh_mount_server 'fo-tp-file'      'root@10.100.0.4'          '/file/'
+ssh_mount_server 'fo-tp-vh01'      'root@10.100.100.201'      '/opt/'
+ssh_mount_server 'fo-tp-install'   'root@10.100.100.200'      '/opt/'
