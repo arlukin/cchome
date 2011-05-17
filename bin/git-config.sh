f340575c79ff0c51406ad7c46e8e4b9d5720c679
Arlukin <daniel@cybercow.se>
2011-05-17 12:11:41 +0200

    changed pathes for git-config

--- bin/git-config.sh
+++ bin/git-config.sh
@@ -5,5 +5,4 @@ git config --global user.email daniel@cybercow.se
 git config --global diff.tool araxis
 git config --global merge.tool araxis
 
-echo "export PATH=$PATH:$HOME/cc/bin" >> $HOME/.bash_profile
-echo "export PATH=$PATH:$HOME/cc/bin/araxis" >> $HOME/.bash_profile
+echo "export PATH=$PATH:$HOME/cchome/bin:$HOME/cchome/bin/araxis" >> $HOME/.bash_profile
