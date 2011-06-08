ba4f4af4196057ffaae11561cf2ad16623417e8d
Daniel Lindh <arlukin@arlukindesktop-av.(none)>
2011-06-08 22:34:52 +0200

    Added configure script of fedora

--- bin/configure-fedora.sh
+++ bin/configure-fedora.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+# cd && curl https://raw.github.com/arlukin/cchome/master/bin/configure-fedora.sh > configure-fedora.sh && chmod +x configure-fedora.sh && ./configure-fedora.sh
+
+# Install cchome
 ssh-keygen -t rsa -C "daniel@cybercow.se"
 cat .ssh/id_rsa.pub
 
-echo -n "Paste the ssh key to github? "
 read DUMMY
+echo -n "Paste the ssh key to github, and press any key."
 
 git clone git@github.com:arlukin/cchome.git
 yum -y install git
+
+# Set monitor to primary screen.
+xrandr --output DVI-0 --primary
+
 
