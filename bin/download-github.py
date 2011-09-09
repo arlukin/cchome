7e5175302f6d02d3b55ca96c7de939889991ef5d
Arlukin <daniel@cybercow.se>
2011-09-09 09:49:52 +0200

    Added comments to download-github.py

--- bin/download-github.py
+++ bin/download-github.py
@@ -18,60 +18,77 @@ import os
 
 git_cmd = "/usr/local/git/bin/git"
 
-def github_get(cmd):
+def _github_get(cmd):
     '''
-    Do a get request against the github api, and convert the 
+    Do a get request against the github api, and convert the
     json result to a python dict
-    '''    
+    '''
     url = "https://api.github.com/" + cmd
     f = urllib.urlopen(url)
     json_data = f.read()
-    return json.loads(json_data)    
-    
-def get_all_orgs(user):
+    return json.loads(json_data)
+
+def _get_all_orgs(user):
     '''
     Return a list with all organizations a user is involved in.
-    '''    
+    '''
     orgs = []
-    for org in github_get("users/" + user + "/orgs"):        
-        orgs.append(org['login'])    
+    for org in _github_get("users/" + user + "/orgs"):
+        orgs.append(org['login'])
     return orgs
 
-def get_all_repos(user):   
+def _get_all_repos(user):
+    '''
+    Return a list with all repos a user owns.
+    
+    Each list item contains a list with name and git clone_url.
+    '''
     clone_urls = []
-    for repo in github_get("users/" + user + "/repos"):
-        clone_urls.append([repo['name'], repo['clone_url']])    
+    for repo in _github_get("users/" + user + "/repos"):
+        clone_urls.append([repo['name'], repo['clone_url']])
     return clone_urls
 
-def git_clone(clone_url, repo_path):
+def _git_clone(clone_url, repo_path):
+    '''
+    Executes git shell command to clone a repo.
+    '''
     cmd =  git_cmd + " clone " + clone_url
     print cmd
     subprocess.Popen(cmd, shell=True, cwd=repo_path).communicate()[0]
-    print   
+    print
 
-def git_pull(clone_url, repo_path):
+def _git_pull(clone_url, repo_path):
+    '''
+    Executes git shell command to pull a repo.
+    '''    
     cmd =  git_cmd + " pull " + clone_url
     print cmd
     print
     subprocess.Popen(cmd, shell=True, cwd=repo_path).communicate()[0]
     print
-                     
-def download_github_repos(user):
-    for name, clone_url in  get_all_repos(user):
+
+def _download_github_repos(user):
+    '''
+    git clone/pull all repos from a user or organizatin.
+    '''    
+    for name, clone_url in  _get_all_repos(user):
         repos_path = os.path.abspath('../github/')
         repo_path = os.path.abspath(repos_path + "/" + name)
         print "---------------------"
         print "Download/Update " + repo_path
         if (os.path.exists(repo_path)):
-            git_pull(clone_url, repo_path)
+            _git_pull(clone_url, repo_path)
         else:
-            git_clone(clone_url, repos_path)
+            _git_clone(clone_url, repos_path)
 
 def download_my_github_repos(user):
-    download_github_repos("arlukin")
-    for org in get_all_orgs("arlukin"):
+    '''
+    git clone/pull all repos from a user and his organizatins.
+    '''    
+    _download_github_repos(user)
+    for org in _get_all_orgs(user):
         print "Download from organization: " + org
-        download_github_repos(org)
-        
+        _download_github_repos(org)
+
 if (__name__ == '__main__'):
-    download_my_github_repos("arlukin")      
+    download_my_github_repos("arlukin")
