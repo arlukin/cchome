8ae546a773adaaf20068cd5d112ccfc4b838244d
Arlukin <daniel@cybercow.se>
2012-01-12 14:14:34 +0100

    OS-x didn't use .profile, it uses .bash_profile

--- bin/setup-osx.sh
+++ bin/setup-osx.sh
@@ -3,10 +3,10 @@
 # How to get UTF8 work properly.
 # http://fruitfulerrors.blogspot.com/2011/01/osx-106-lcctype.html
 # http://hints.macworld.com/article.php?story=20060825071728278
-if grep -q "LC_CTYPE=en_US.UTF-8" ~/.profile 
+if grep -q "LC_CTYPE=en_US.UTF-8" ~/.bash_profile 
 then
 	echo "LC_CTYPE already set"
 else
-	echo "export LC_CTYPE=en_US.UTF-8" >> ~/.profile
+	echo "export LC_CTYPE=en_US.UTF-8" >> ~/.bash_profile
 	export LC_CTYPE=en_US.UTF-8
 fi
