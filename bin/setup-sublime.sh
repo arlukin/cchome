a14436fdbac5b87f5be45a0f04db07661f0eda72
Arlukin <daniel@cybercow.se>
2012-01-29 22:36:50 +0100

    Added symlink /bin/subl for sublime

--- bin/setup-sublime.sh
+++ bin/setup-sublime.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd ~/Library/Application\ Support/Sublime\ Text\ 2/Packages
 git clone git://github.com/lunixbochs/sublimelint.git
+
+sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /bin/subl
